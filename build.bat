6dc24832031044c56c49c497384e7dc0c7d59144
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2022-08-27 00:22:01 +0200

    Setup basic 3D OpenGL and math libs

---
+++ build.bat
@@ -0,0 +1,30 @@
+@echo off
+
+SETLOCAL
+
+REM cl flags
+set CLFLAGS=/W4 /Zi /nologo /MD /MP4 /Fecornellbox.exe
+set CLFLAGS=%CLFLAGS% /Iw:\cornell_box_opengl\libs /Iw:\cornell_box_opengl\libs\glfw\include /Iw:\cornell_box_opengl\libs\glm
+set LINKFLAGS=glfw3.lib opengl32.lib shell32.lib gdi32.lib user32.lib kernel32.lib /link
+set LINKFLAGS=%LINKFLAGS% /LIBPATH:w:\cornell_box_opengl\libs\glfw\lib-vc2022
+
+set CFILES=
+
+REM Create build directory if it doesnt exist already
+IF NOT EXIST ".\build\" mkdir build
+
+REM Get all .cpp files from code dir as well from GLAD source
+pushd ".\code"
+for /R %%c in (*.cpp) do call set CFILES=%%c %%CFILES%%
+popd
+
+pushd ".\libs\glad"
+for /R %%c in (*.c) do call set CFILES=%%c %%CFILES%%
+popd
+
+REM Compile
+pushd ".\build\"
+cl %CLFLAGS% %CFILES% %LINKFLAGS%
+popd
+
+ENDLOCAL
